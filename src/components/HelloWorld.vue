12bde5c905a21334b52919afcacf043ec5e830e2
aditya gupta <adityasgupta309@gmail.com>
2020-07-06 20:52:47 +0530

    vuex and vuelidate demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p v-for="notification in notifications" :key="notification">
+      {{notification}}
+    </p>
+    <button @click="addHey">Add Hey!</button>
+  </div>
+</template>
+
+<script>
+import state from '@/state'
+
+export default {
+  created () {
+    this.$store.registerModule('notifications', state)
+  },
+  computed: {
+    notifications () {
+      return this.$store.state.notifications
+    }
+  },
+  methods: {
+    addHey () {
+      this.$store.commit('addNotification', 'Hey!')
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Loading Vuex Modules Dynamically</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/><hr/>
+    <BasicForm/>
+    <Formsubmission/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import BasicForm from '@/components/BasicForm.vue'
+import Formsubmission from '@/components/Formsubmission.vue'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    BasicForm,
+    Formsubmission
+  }
+}
+</script>
